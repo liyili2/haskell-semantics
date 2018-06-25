d584aeaf7e38324c92b40512f7eb47ace2f80bad
Bradley Morrell <brrmorre@gmail.com>
2018-06-25 18:25:24 -0500

    added test5

---
+++ bradstests/Simp5.hs
@@ -0,0 +1,12 @@
+module Simp4 where
+
+type Name = String
+
+; data Anniversary = 
+   Birthday Name Date
+   | Wedding Name Name Date
+
+; data Date = Date Int Int Int
+
+; johnSmith :: Anniversary
+; johnSmith = Birthday "John Smith" (Date 1968 7 3)
