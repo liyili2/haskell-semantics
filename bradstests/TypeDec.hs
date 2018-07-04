1950be3307ffaba464eeada44e52898007083198
Bradley Morrell <brrmorre@gmail.com>
2018-07-04 14:36:26 -0500

    added fixity and another test

---
+++ bradstests/TypeDec.hs
@@ -0,0 +1,30 @@
+module TypeDec where
+
+{ type Name = String
+
+; data Anniversary = 
+   Birthday Name Date
+   | Wedding Name Name Date
+
+; data Date = Date Int Int Int
+
+; johnSmith :: Anniversary
+; johnSmith = Birthday "John Smith" (Date 1968 7 3)
+
+; smithWedding :: Anniversary
+; smithWedding = Wedding "John Smith" "Jane Smith" (Date 1987 3 4)
+
+; type AnniversaryBook = [Anniversary]
+
+; anniversariesOfJohnSmith :: AnniversaryBook
+; anniversariesOfJohnSmith = [johnSmith, smithWedding]
+
+; showDate :: Date -> String
+; showDate (Date y m d) = show y ++ "-" ++ show m ++ "-" ++ show d 
+
+; showAnniversary :: Anniversary -> String
+; showAnniversary (Birthday name date) =
+   name ++ " born " ++ showDate date
+; showAnniversary (Wedding name1 name2 date) =
+   name1 ++ " married " ++ name2 ++ " on " ++ showDate date
+}
